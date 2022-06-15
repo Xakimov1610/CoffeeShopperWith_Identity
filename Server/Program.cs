072d6d32903bfeb8f6d2b580caee4defb27d326e
Xakimov1610 <nosirbekxakimov01@gmail.com>
2022-06-15 05:55:02 +0500

    Settings IdentityServer4

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var assebly = typeof(Program).Assembly.GetName().Name;
 var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
+    options.UseSqlServer(defaultConnString,
+        b => b.MigrationsAssembly(assebly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
     .AddConfigurationStore(options =>
     {
         options.ConfigureDbContext = b =>
